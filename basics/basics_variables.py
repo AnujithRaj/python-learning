8c3cc3e62473fde4b448db2d2c1eb18f96f87a52
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-01 15:39:36 +0530

    Added Basics Variables files

---
+++ basics/basics_variables.py
@@ -0,0 +1,69 @@
+# Create a variable name and store your name in it. Print it.
+name = "Anujith"
+
+print("Name:", name)
+
+# Create a variable age and store your age. Print it.
+age = 22
+
+print("Age: ", age)
+
+# Store your college name in a variable and print it.
+college = "Manipal University"
+
+print("College: ", college)
+
+# Create variables x = 10 and y = 20. Print both.
+x = 10
+y = 20
+
+print("X = ", x, "Y = ", y)
+
+
+# Store a decimal number in a variable and print it.
+d = 20.11
+
+print("Decimal Number: ", d)
+
+
+# Store a boolean value (True or False) and print it.
+young = True
+old = False
+
+print("Boolean Value young: ", young)
+print("Boolean value old: ", old)
+
+
+# Create three variables and print them in a single line.
+a = 11
+b = 20
+c = 30
+
+print("Three Variables are: ", a, b, c)
+
+
+# Assign the same value to three variables.
+num1 = num2 = num3 = 10
+
+print(num1, num2, num3)
+
+
+# Swap the values of two variables.
+p = 30
+q = 50
+p, q = q,p
+
+print("p = ", p)
+print("q = ",q)
+
+
+# Check the data type of different variables using type()
+a = 10
+b = 20.11
+c = True
+d = "Siddharth"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
