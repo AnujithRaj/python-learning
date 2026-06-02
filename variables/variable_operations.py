acb5991516c71933f6322ac1123e29f63fe80126
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-02 14:57:51 +0530

    Add Variable Operations

---
+++ variables/variable_operations.py
@@ -0,0 +1,69 @@
+import math
+# Store two numbers and print their sum.
+num1 = 20
+num2 = 30
+
+print("Sum: ",num1 + num2)
+
+
+# Store two numbers and print their difference.
+a = 10
+b = 5
+
+print("Difference: ", a - b)
+
+
+# Store two numbers and print their product.
+x = 12
+y = 6
+
+print("Product: ", x * y)
+
+# Store two numbers and print their division.
+p = 40
+q = 5
+
+print("Division: ", p / q)
+
+
+# Find the remainder of two numbers.
+number1 = 5
+number2 = 2
+
+print("Remainder: ", number1 % number2)
+
+# Calculate the square of a number.
+sqr1 = 8
+
+print("Square: ", sqr1 ** 2)
+
+
+# Calculate the cube of a number.
+cub1 = 3
+
+print("Cube: ", cub1 ** 3)
+
+# Store length and width, then calculate area of a rectangle.
+length = 10
+width = 20
+
+area = length * width
+
+print("Area of Rectangle: ", area)
+
+# Store radius and calculate area of a circle.
+radius = 21
+
+area = math.pi * radius * radius
+
+print("Area of Circle: ", area)
+
+
+# Store principal, rate and time, then calculate simple interest.
+principal = 1000
+rate = 8
+time= 12
+
+simple_interest = principal * rate * time / 100
+
+print("Simple Interest: ", simple_interest)
