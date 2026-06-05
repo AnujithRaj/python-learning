96803335e614bd0a6e91e9d7ee3df3d96179f738
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-05 08:17:13 +0530

    Add Variables Manipulation

---
+++ variables/variable_manipulation.py
@@ -0,0 +1,65 @@
+# Store a string and print it in uppercase.
+str1 = "Welcome!"
+
+print(str1.upper())
+
+
+# Store a string and print in lowercase.
+str2 = "Hello"
+
+print(str2.lower())
+
+
+# Find the length of a string stored in a variable.
+str3 = "Hello World!"
+
+print(len(str3))
+
+
+# Concatenate first name and last name.
+f_name = "Siddharth"
+l_name = "Kumar"
+
+full_name = f_name + " " + l_name
+
+print(full_name)
+
+
+# Repeat a string 5 times.
+word = "Hii"
+
+print(word * 5)
+
+
+# Store a sentence and count characters.
+sentence = "I am learning Python"
+
+print(len(sentence))
+
+
+# Extract the first character of a string.
+text1 = "Python"
+
+print(text1[0])
+
+
+# Extract the last character of a string.
+text2 = "Learning"
+
+print(text2[-1])
+
+
+# Reverse a string using slicing.
+name = "Prita"
+
+print(name[::-1])
+
+
+# Check whether two variables contain the same value.
+a = "Python"
+b = "Python"
+c = "Java"
+
+print(a == b)
+
+print(a == c)
