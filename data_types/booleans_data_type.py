9cba75821f698327d251840a0c316a1aa79baf6e
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-09 08:33:14 +0530

    Implement Booleans Data Types in Python

---
+++ data_types/booleans_data_type.py
@@ -0,0 +1,38 @@
+# Create True and False variables.
+a = True
+b = False
+
+print(a)
+print(b)
+
+
+# Compare two numbers using >.
+number1 = int(input("Enter First Number: "))
+number2 = int(input("Enter Second Number: "))
+
+print(number1 > number2)
+
+
+# Compare two numbers using ==.
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print(num1 == num2)
+
+
+# Check if a number is positive.
+num = int(input("Enter Number: "))
+
+if num >= 0:
+    print("positive Number")
+else:
+    print("Negative Number")
+
+
+# Check if a string is empty.
+str1 = input("Enter a String: ")
+
+if str1 == "":
+    print("String is empty")
+else:
+    print("String is not empty")
