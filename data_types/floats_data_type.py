0417a66c3d7610f234151b3be2ef2b27b19b4d69
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-10 15:27:14 +0530

    Add Float Data Type

---
+++ data_types/floats_data_type.py
@@ -0,0 +1,38 @@
+# Store your height in meters.
+height = 1.7
+
+print("Height:", height)
+
+
+# Multiply a float by an integer.
+age = 24
+weight = 64.8
+
+multi = age * weight
+
+print("Float Multiplication: ", multi)
+
+
+# Round a float to 2 decimal places.
+num = 12.3456
+
+round_num = round(num, 2)
+
+print("Rounded Number:", round_num)
+
+
+# Convert an integer to float.
+integer_num = 25
+
+float_num = float(integer_num)
+
+print("Float Number:", float_num)
+print("Type:", type(float_num))
+
+
+# Find the area of a circle using a float radius.
+radius = float(input("Enter Radius: "))
+
+area= 3.14 * radius ** 2
+
+print("Area of a Circle:", area)
