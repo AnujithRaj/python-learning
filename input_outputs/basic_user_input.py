0365c53b6510291c32eb941c6a0af1926b12df3b
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-14 14:18:30 +0530

    Implement Basic User Input

---
+++ input_outputs/basic_user_input.py
@@ -0,0 +1,28 @@
+# Take a name as input and print.
+name = str(input("Enter Your Name: "))
+
+print("Hello,", name)
+
+
+# Take your age as input and print.
+age = int(input("Enter your Age: "))
+
+print("You are", age, "Years Old")
+
+
+# Take a city name as input and display.
+city = str(input("Enter Your City: "))
+
+print("I Live in", city)
+
+
+# Take a favorate color as input and print it.
+fav_color = str(input("Enter Your Favorate Color: "))
+
+print("Favorate color:", fav_color)
+
+
+# Take a number as input and print.
+num = int(input("Enter Number: "))
+
+print("The Entered Number is:", num)
