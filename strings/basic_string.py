1cedd79bc42bfea762bb467a53eac0cfc5280662
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-16 15:47:26 +0530

    Add Basic String problem

---
+++ strings/basic_string.py
@@ -0,0 +1,19 @@
+# Create a string containing your name and print it.
+name = input("Enter Name: ")
+
+print("Name:", name)
+
+
+# Find the length of a string.
+print("Length:", len(name))
+
+
+# Access the first and last character of a string.
+
+# Print each character of a string using indexing.
+# Concatenate two strings.
+# Repeat a string 5 times.
+# Check whether a character exists in a string.
+# Take a string as input and print it in uppercase.
+# Take a string as input and print it in lowercase.
+# Reverse a string using slicing.
