e4ed8962d462b5a1dcb97c1a99f9746df2620b7a
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-18 15:19:22 +0530

    Add Mixed Input Output

---
+++ input_outputs/mixed_input.py
@@ -0,0 +1,33 @@
+# Take name and age and print:
+name = str(input("Enter Name: "))
+age = int(input("Enter Age: "))
+
+print(f"Name: {name}, Age: {age}")
+
+
+# Take product name and price and display a bill.
+product = input("Enter Product Name: ")
+price = float(input("Enter Product Price: "))
+
+print(f"Product: {product}, Price: {price}")
+
+
+# Take student name and marks and print:
+student_name = input("Enter Your Name: ")
+marks = int(input("Enter Your Marks: "))
+
+print(f"Student Name: {student_name}, Marks: {marks}")
+
+
+# Take employee name and salary and print details.
+emp_name = input("Enter Your Name: ")
+salary = int(input("Enter Your Salary: "))
+
+print(f"Employee Name: {emp_name}, Salary: {salary}")
+
+
+# Take movie name and rating and display them.
+movies = input("Enter Movies Name: ")
+rating = float(input("Rate this Movies Out of 10: "))
+
+print(f"Movies Name: {movies}, Rating: {rating}")
