ab1764f4c18f95aefe03d71db817d51855723a8d
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-06-18 16:14:22 +0530

    Add Basic Type Conversion Problem

---
+++ type_casting/basic_type_conversion.py
@@ -0,0 +1,74 @@
+# Convert the string "100" into an integer.
+str1 = "100"
+
+int1 = int(str1)
+
+print(type(int1))
+
+
+# Convert the integer 25 into a float.
+int2 = 25
+
+float1 = float(int2)
+
+print(type(float1))
+
+
+# Convert the float 7.89 into an integer.
+float2 = 7.89
+
+int3 = int(float2)
+
+print(int3, type(int3))
+
+
+# Convert the integer 50 into a string.
+int4 = 50
+
+str2 = str(int4)
+
+print(str2, type(str2))
+
+
+# Convert the string "3.14" into a float.
+str3 = "3.14"
+
+float5 = float(str3)
+
+print(float5, type(float5))
+
+
+# Convert the boolean value True into an integer.
+bool_value = True
+
+int_value = int(bool_value)
+
+print(int_value)
+print(type(int_value))
+
+
+# Convert the boolean value False into an integer.
+bool_value1 = False
+
+int_value1 = int(bool_value1)
+
+print(int_value1)
+print(type(int_value1))
+
+
+# Convert the integer 1 into a boolean.
+int_val = 1
+
+bool_val = bool(int_val)
+
+print(bool_val)
+print(type(bool_val))
+
+
+# Convert the integer 0 into a boolean.
+int_val1= 0
+
+bool_val1 = bool(int_val1)
+
+print(bool_val1)
+print(type(bool_val1))
